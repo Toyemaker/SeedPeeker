828b7aa59252ea0a6cf1481ba609d818ae99a221
Sen Kappa <SenKappa@users.noreply.github.com>
2022-10-25 14:43:28 -0400

    Experiments

--- SeedPeeker/SetSeededRunOverridePatch.cs
+++ SeedPeeker/SetSeededRunOverridePatch.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Unity.Collections;
 using Unity.Entities;
+using static SeedPeeker.SeedPeekerMod;
 
 namespace SeedPeeker
 {
@@ -21,14 +22,50 @@ namespace SeedPeeker
             public UnlockChain Chain2;
 
             public Unlock Unlock;
+            public HashSet<int> CurrentUnlockIDs = new HashSet<int>();
+
+            public int Step;
+            public int Index;
         }
 
         public static HashSet<int> CurrentUnlockIDs = new HashSet<int>();
         public static List<CProgressionRequest> Requests;
+        public static Seed Seed;
+        public static UnlockChain Root;
 
         [HarmonyPatch("CreateSeededRun")]
         [HarmonyPostfix]
         public static void CreateSeededRun_PostFix(Seed seed)
+        {
+            Seed = seed;
+
+            Recurse(null, 0, 0);
+        }
+
+        public static UnlockChain Recurse(UnlockChain chain, Unlock unlock, int step, int index)
+        {
+            UnlockChain chain = new UnlockChain()
+            {
+                Step = step,
+                Index = index,
+                Unlock = unlock,
+
+            };
+
+            if(step < 3)
+            {
+                Unlock[] unlocks = GetNextUnlocks();
+
+                chain.Chain1 = Recurse(chain, unlocks[0], 1, index * 2);
+                chain.Chain2 = Recurse(chain, unlocks[1], 1, index * 2 + 1);
+            }
+            else
+            {
+                return chain;
+            }
+        }
+
+        public static Unlock[] GetNextUnlocks()
         {
             int category_seed = 848292;
             int instance = 3;
@@ -37,7 +74,7 @@ namespace SeedPeeker
 
 
             UnityEngine.Random.State cachedState = UnityEngine.Random.state;
-            UnityEngine.Random.InitState(seed.IntValue + 124192293 * instance_seed);
+            UnityEngine.Random.InitState(Seed.IntValue + 124192293 * instance_seed);
 
             bool choose_bias_option = UnityEngine.Random.value > 0.5f;
 
@@ -47,29 +84,30 @@ namespace SeedPeeker
                 new CProgressionRequest() {Group = UnlockGroup.Dish},
             };
 
+            int i = 0;
+            Unlock[] unlocks = new Unlock[2];
             foreach (CProgressionRequest request in Requests)
             {
-                if (!AddUnlock(request.Group, choose_bias_option, 0))
+                if (!AddUnlock(request.Group, choose_bias_option, 0, out unlocks[i++]))
                 {
+                    i--;
                     foreach (CProgressionRequest request2 in Requests)
                     {
                         if (request.Group != request2.Group)
                         {
-                            AddUnlock(request2.Group, false, 0);
+                            DebugLogger.LogInfo("unlock2");
+                            AddUnlock(request2.Group, false, 0, out unlocks[i++]);
                         }
                     }
                 }
             }
 
             UnityEngine.Random.state = cachedState;
+
+            return unlocks;
         }
 
-        public static UnlockChain Recurse(UnlockChain chain, int step)
-        {
-            // base case: 
-        }
-
-        public static bool AddUnlock(UnlockGroup group, bool choose_bias_option, int tier)
+        public static bool AddUnlock(UnlockGroup group, bool choose_bias_option, int tier, out Unlock returnUnlock)
         {
             List<Unlock> unlocks = GetUnlocks(group, choose_bias_option, tier);
             foreach (Unlock unlock in unlocks)
@@ -83,13 +121,14 @@ namespace SeedPeeker
                         bool flag3 = unlock.BlockedBy.Any((Unlock r) => CurrentUnlockIDs.Contains(r.ID));
                         if (!flag3)
                         {
-                            CurrentUnlockIDs.Add(unlock.ID);
+                            returnUnlock = unlock;
                             SeedPeekerMod.DebugLogger.LogInfo("Potential ID: " + unlock.ID);
                             return true;
                         }
                     }
                 }
             }
+            returnUnlock = default;
             return false;
         }
 
